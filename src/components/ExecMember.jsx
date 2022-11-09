37130244ae612e0d17198654740c0aa914b1f4cf
Hao C <impyworm@gmail.com>
2022-11-08 21:39:16 -0700

    Complete v1 website

--- src/components/ExecMember.jsx
+++ src/components/ExecMember.jsx
@@ -6,22 +6,42 @@ export default function ExecMember({
   name,
   title,
   image,
-  quote,
-  orientation,
-  breakLine,
+  // quote,
+  // orientation,
+  // breakLine,
   id,
 }) {
   return (
     <Zoom delay={id * 100}>
-      <div className='w-full px-1'>
+      <div className='w-1/5 px-1'>
         <div className='flex flex-col'>
-          {/* <img
+          <img
+            className='rounded-md border-2 border-neutral-800 drop-shadow-md transition-all delay-100 duration-200'
+            src={image}
+            alt='Executive team images'
+          />
+          <div className='mt-4 text-center'>
+            <h1 className='mb-1 font-montserrat text-xs font-bold text-gray-100'>
+              {name}
+            </h1>
+            {title && (
+              <div className='mb-2 text-xs font-light text-gray-300'>
+                {title}
+              </div>
+            )}
+          </div>
+          
+          {/* <div className='w-full px-1'>
+        <div className='flex flex-col'>
+          <img
             className='rounded-md border-2 border-neutral-800 drop-shadow-md transition-all delay-100 duration-500 hover:drop-shadow-xl'
             src={image}
             alt='Executive team images'
           />
           <div className='mt-4 text-center'>
-            <h1 className='mb-1 text-xs font-bold text-gray-100 font-montserrat'>{name}</h1>
+            <h1 className='mb-1 font-montserrat text-xs font-bold text-gray-100'>
+              {name}
+            </h1>
             {title && (
               <div className='mb-2 text-xs font-light text-gray-300'>
                 {title}
@@ -29,19 +49,17 @@ export default function ExecMember({
             )}
           </div> */}
 
-          {/* bg-gradient-to-r from-sky-300 to-emerald-300 p-[2px] */}
-
-          {orientation == 'right' ? (
+          {/* {orientation == 'right' ? (
             <div className='flex flex-row items-center justify-center gap-12'>
               <div className='flex flex-col items-end justify-center'>
                 <h2 className='font-montserrat text-3xl text-gray-100'>
                   {name}
                 </h2>
                 <span className='text-lg text-gray-100'>{title}</span>
-                <p className='text-right text-lg text-gray-100/60'>{quote}</p>
+                <p className='text-right text-gray-100/60'>{quote}</p>
               </div>
               <img
-                className='w-[12rem] rounded-md'
+                className='w-[15rem] rounded-md'
                 src={image}
                 alt='Executive team images'
               />
@@ -63,7 +81,7 @@ export default function ExecMember({
             </div>
           )}
 
-          {breakLine && <hr className='m-auto my-10 w-24 border-gray-100/60' />}
+          {breakLine && <hr className='m-auto my-10 w-24 border-gray-100/60' />} */}
         </div>
       </div>
     </Zoom>
